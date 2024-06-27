cbec54022f2019d654b78a73c9681f6a548596b0
tajemouti <tajemouti@gmail.com>
2024-06-27 20:58:03 +0100

    Render Fetched products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Button, Card } from 'react-bootstrap';
 
 const Product = () => {
   const [products, getProducts] = useState([]);
@@ -10,10 +11,32 @@ const Product = () => {
 
   }, []);
 
+  const cards = products.map(product => [
+    <div className="col-md-3" style={{marginBottom: '10px'}}>
+      <Card key={product.id} className="h-100">
+        <div className='text-center'>
+          <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>
+            {product.price}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: 'white'}}>
+          <Button variant="primary">Add To Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+
+  ])
+
   return (
     <>
-      <h1>Product</h1>
-      {JSON.stringify(products)}
+      <h1>Products</h1>
+      <div className="row">
+        {cards}
+      </div>
     </>
   )
 }
