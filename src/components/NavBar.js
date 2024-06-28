0d865ea3554321e2db2c0918cb177a701575ac4f
tajemouti <tajemouti@gmail.com>
2024-06-28 12:34:29 +0100

    Implement HTML structure for navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">ٌRedux ToolKit</Navbar.Brand>
+        <Nav>
+            <Nav.Link to="/" as={Link}>Products</Nav.Link>
+          </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Nav.Link to="/cart" as={Link}>My bag 0</Nav.Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
