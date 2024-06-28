43b560478b608f234ea82e62f6aa76aae0a2686c
tajemouti <tajemouti@gmail.com>
2024-06-28 11:31:26 +0100

    Setup routes in the main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import Product from './components/Product';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Cart from './components/Cart';
+import RootLayout from './components/RootLayout';
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Dashboard />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+    </Route>
+  ))
+
   return (
     <div className="App">
-      <Product />
+      <RouterProvider router={router}/>
     </div>
   );
 }
