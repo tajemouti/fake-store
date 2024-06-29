c2df0b42e07e1480f99e16fcd586a3ae3a5cfbc4
tajemouti <tajemouti@gmail.com>
2024-06-29 14:52:13 +0100

    Import productSlice to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from '../features/cartSlice';
+import productSlice from '../features/productSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    products: productSlice,
   },
 });
 
