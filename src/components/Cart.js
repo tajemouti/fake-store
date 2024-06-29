64d9b9e58b629beb0f4e02b9a2cff9e1e31e163a
tajemouti <tajemouti@gmail.com>
2024-06-29 13:01:46 +0100

    Define HTML struture for Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
+import { remove } from '../features/cartSlice';
 
 const Cart = () => {
+  const products = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+  const removeFromCart = (id) => {
+    dispatch(remove(id));
+  }
+
+  const cards = products.map(product => [
+    <div key={product.id} className="col-md-12" style={{marginBottom: '10px'}}>
+      <Card className="h-100">
+        <div className='text-center'>
+          <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>
+            {product.price}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: 'white'}}>
+          <Button variant="danger" onClick={() => removeFromCart(product.id)}>Remove</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ])
+
   return (
-    <div>Cart</div>
+      <div className="row">
+        {cards}
+      </div>
   )
 }
 
