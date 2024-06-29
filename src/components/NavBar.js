803f64cf8e824c64cb69b17770035fa003600d8e
tajemouti <tajemouti@gmail.com>
2024-06-29 19:07:46 +0100

    Fix linters' errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  const count = useSelector(state => state.cart.length)
+  const count = useSelector((state) => state.cart.length);
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#">ٌRedux ToolKit</Navbar.Brand>
         <Nav>
-            <Nav.Link to="/" as={Link}>Products</Nav.Link>
-          </Nav>
+          <Nav.Link to="/" as={Link}>Products</Nav.Link>
+        </Nav>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            <Nav.Link to="/cart" as={Link}>My bag {count}</Nav.Link>
+            <Nav.Link to="/cart" as={Link}>
+              My bag
+              {count}
+            </Nav.Link>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -1,35 +1,35 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    data: [],
-    status: 'idle',
+  data: [],
+  status: 'idle',
 };
 
-const productSlice = createSlice({
-    name: 'products',
-    initialState,
-    reducers: {
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getProducts.pending, (state) => {
-                state.status = 'loading';
-            })
-            .addCase(getProducts.fulfilled, (state, action) => {
-                state.data = action.payload;
-                state.status = 'idle';
-            })
-            .addCase(getProducts.rejected, (state) => {
-                state.status = 'error';
-            })
-    }
+export const getProducts = createAsyncThunk('products/get', async () => {
+  const data = await fetch('https://fakestoreapi.com/products');
+  const result = await data.json();
+
+  return result;
 });
 
-export const getProducts = createAsyncThunk('products/get', async () => {
-    const data = await fetch('https://fakestoreapi.com/products');
-    const result = await data.json();
-
-    return result;
-})
+const productSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = 'idle';
+      })
+      .addCase(getProducts.rejected, (state) => {
+        state.status = 'error';
+      });
+  },
+});
 
 export default productSlice.reducer;
