580b33648c366c4734afd402c8092a209adefa20
tajemouti <tajemouti@gmail.com>
2024-06-29 14:54:33 +0100

    Add conditional rendering to Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Card } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react'
+import { Alert, Button, Card } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../features/cartSlice';
+import { getProducts } from '../features/productSlice';
 
 const Product = () => {
   const dispatch = useDispatch();
 
-  const [products, getProducts] = useState([]);
+  const products = useSelector(state => state.products.data);
+  const status = useSelector(state => state.products.status);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then(data => data.json())
-      .then(result => getProducts(result))
-
-  }, []);
+    dispatch(getProducts());
+  }, [dispatch]);
 
   const addToCart = (product) => {
     dispatch(add(product));
   }
 
+  if (status === 'loading') {
+    return <div>loading...</div>
+  }
+
+  if (status === 'error') {
+    return <Alert key='danger' variant='danger'>
+      Something went wrong!! Try again later
+    </Alert>
+  }
+
   const cards = products.map(product => [
-    <div key={product.id} className="col-md-3" style={{marginBottom: '10px'}}>
+    <div key={product.id} className="col-md-3" style={{ marginBottom: '10px' }}>
       <Card className="h-100">
         <div className='text-center'>
           <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
@@ -31,8 +40,8 @@ const Product = () => {
             {product.price}
           </Card.Text>
         </Card.Body>
-        <Card.Footer style={{ background: 'white'}}>
-          <Button variant="primary" onClick={()=>{addToCart(product)}}>Add To Cart</Button>
+        <Card.Footer style={{ background: 'white' }}>
+          <Button variant="primary" onClick={() => { addToCart(product) }}>Add To Cart</Button>
         </Card.Footer>
       </Card>
     </div>
